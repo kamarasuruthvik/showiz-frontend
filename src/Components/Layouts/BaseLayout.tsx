49f02e447fda88f4284a1d2f6c51c03d8471f32b
Sameer Ali Hannan <60455498+Sameer0604@users.noreply.github.com>
2023-12-04 23:16:44 -0800

    added header component

--- src/Components/Layouts/BaseLayout.tsx
+++ src/Components/Layouts/BaseLayout.tsx
@@ -1,25 +1,59 @@
-import { AppShell, Container, Button, CloseButton, Flex, Stack, NavLink } from '@mantine/core';
-import { IconBrandGoogleFilled, IconChevronRight, IconGauge } from '@tabler/icons-react';
-import { useDisclosure } from '@mantine/hooks';
+import { AppShell, Container, Group, } from '@mantine/core';
+
+import classes from './header.module.css';
+
+import ShowizLogo from './showizLogo.png';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+
+const links = [
+  { link: '/about', label: 'About' },
+  { link: '/pricing', label: 'Pricing' },
+  { link: '/learn', label: 'Learn' },
+  { link: '/community', label: 'Community' },
+]
 
 export function BaseLayout(props: React.PropsWithChildren<any>) {
+  const navigate = useNavigate();
+  const [active, setActive] = useState(links[0].link);
+
+  const items = links.map((link) => (
+    <a
+      key={link.label}
+      href={link.link}
+      className={classes.link}
+      data-active={active === link.link || undefined}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(link.link);
+      }}
+    >
+      {link.label}
+    </a>
+  ));
 
   return (
     <AppShell
-    header={{ height: { base: 48, sm: 60, lg: 76 } }}
+      header={{ height: { base: 48, sm: 60, lg: 76 } }}
     >
       <AppShell.Header>
-        <Container px={0} size="40rem">
-          <IconBrandGoogleFilled />
+        <Container size="md" className={classes.inner}>
+          <div onClick={() => navigate('/home')} style={{ display: 'flex' }}>
+            <img src={ShowizLogo} alt='Showiz' className={classes.headerIcon} />
+          </div>
+          <Group gap={20} visibleFrom="md">
+            {items}
+          </Group>
         </Container>
       </AppShell.Header>
       <AppShell.Main>
-        <div style={{ margin:"auto"}}>
-          {props.children} 
+        <div style={{ margin: "auto" }}>
+          {props.children}
         </div>
       </AppShell.Main>
     </AppShell>
-  );                                       
+  );
 }
 
 export default BaseLayout;
